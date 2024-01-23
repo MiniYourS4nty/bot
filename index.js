1c544337416ea7052fad2161ce10cda3a0964f6f
Santiago Gonzalez <156966174+MiniYourS4nty@users.noreply.github.com>
2024-01-22 20:33:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,39 +2,44 @@
 require('./config')
 
 // Console Decoration
-const fs = require('fs')
-const CFonts = require ('cfonts');
-const {spawn} = require ('child_process');
-let path = require('path')
+const fs = require('fs');
+const CFonts = require('cfonts');
+const { spawn } = require('child_process');
+const path = require('path');
 
 CFonts.say('Moon - Wabot', {
   font: 'chrome',
-  align: 'center',  
-  gradient: ['blue', 'cyan']
-})
+  align: 'center',
+  gradient: ['blue', 'cyan'],
+});
 
 // Starting The Main Script
 function start() {
-  let args = [path.join(__dirname, 'HandlerEvents.js'), ...process.argv.slice(2)]
+  // Use the PORT environment variable provided by Heroku, or default to 3000 if running locally
+  const port = process.env.PORT || 3000;
+
+  // Pass the port as an argument to HandlerEvents.js
+  let args = [path.join(__dirname, 'HandlerEvents.js'), `--port=${port}`, ...process.argv.slice(2)];
+
   let p = spawn(process.argv[0], args, {
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
   })
-  .on('message', data => {
+  .on('message', (data) => {
     if (data == 'reset') {
-      console.log('RESET')
-      p.kill()
-      start()
-      delete p
+      console.log('RESET');
+      p.kill();
+      start();
+      delete p;
     }
-  })
+  });
 }
 
-start()
+start();
 
-let file = require.resolve(__filename)
+let file = require.resolve(__filename);
 fs.watchFile(file, () => {
-    fs.unwatchFile(file)
-    console.log(chalk.redBright(`File Updated : ${__filename}`))
-    delete require.cache[file]
-    require(file)
-})
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`File Updated: ${__filename}`));
+  delete require.cache[file];
+  require(file);
+});
